767a6897f49f4f4624fbf8894f18a32fa2c4ca51
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2025-03-02 16:35:26 +0900

    移動、BEEP、ライトを実装。

--- main.ts
+++ main.ts
@@ -1,8 +1,50 @@
+makerbit.onIrButton(IrButton.BEEP, IrButtonAction.Released, function () {
+    music.stopAllSounds()
+})
+makerbit.onIrButton(IrButton.Right, IrButtonAction.Released, function () {
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.TLeft, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 80)
+})
+makerbit.onIrButton(IrButton.Right, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Right, 80)
+})
+makerbit.onIrButton(IrButton.Left, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Left, 80)
+})
+makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 80)
+})
 makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
-    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 40)
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 80)
+})
+makerbit.onIrButton(IrButton.TRight, IrButtonAction.Released, function () {
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.Left, IrButtonAction.Released, function () {
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.Power, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
+})
+makerbit.onIrButton(IrButton.Down, IrButtonAction.Released, function () {
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.BEEP, IrButtonAction.Pressed, function () {
+    music.ringTone(349)
+})
+makerbit.onIrButton(IrButton.TRight, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 80)
 })
 makerbit.onIrButton(IrButton.Up, IrButtonAction.Released, function () {
     Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.TLeft, IrButtonAction.Released, function () {
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.Plus, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Blue)
 })
 makerbit.connectIrReceiver(DigitalPin.P8)
 basic.forever(function () {
